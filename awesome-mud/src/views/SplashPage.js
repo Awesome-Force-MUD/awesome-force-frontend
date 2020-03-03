959166eeb0b33f43af28fb014bf0816f7b41651a
Kristin Barr <kristinbarr@gmail.com>
2020-03-03 18:33:44 -0500

    splash page styling

--- awesome-mud/src/views/SplashPage.js
+++ awesome-mud/src/views/SplashPage.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import space from "../components/assets/space.png"
+import style from "styled-components"
+import styled from 'styled-components'
 
 const SplashPage = () => {
     return(
-        <div className="splash-page">
-            <h1>Welcome to Awesome MUD</h1>
-            <img src={space} />
-        </div>
+        <SplashContainer>
+            <h1>Welcome to the
+            <br/> 
+            AWESOME FORCE MUD GAME!</h1>
+            <br/>
+            <h3>Log in or register to continue...</h3>
+        </SplashContainer>
     )
 }
 
 export default SplashPage
+
+
+const SplashContainer = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+    padding-top: 10rem;
+    color: white;
+    background: url(${space});
+    min-height: calc(100vh - 130px);
+    width: 100%;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    overflow: hidden;
+
+    h1 {
+        margin: 0;
+        font-size: 4rem;
+    }
+    
+    h3 {
+        font-size: 2rem;
+        padding-top: 1rem;
+    }
+
+`

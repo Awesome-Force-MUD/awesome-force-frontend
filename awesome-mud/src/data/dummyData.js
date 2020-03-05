3ce67622eff613ed299c74ac377857b2b538e83b
Kristin Barr <kristinbarr@gmail.com>
2020-03-05 16:03:47 -0500

    dummy data

---
+++ awesome-mud/src/data/dummyData.js
@@ -0,0 +1,146 @@
+const data = {
+  room: [
+    {
+      title: "Outside Cave Entrance",
+      description: "North of you, the cave mount beckons",
+      n_to: 496,
+      s_to: -1,
+      e_to: -1,
+      w_to: -1,
+      loc_x: 127,
+      loc_y: 20
+    },
+    {
+      title: "Foyer",
+      description:
+        "Dim light filters in from the south. Dusty\npassages run north and east.",
+      n_to: 497,
+      s_to: 495,
+      e_to: 1124,
+      w_to: 1126,
+      loc_x: 128,
+      loc_y: 20
+    },
+    {
+      title: "Grand Overlook",
+      description:
+        "A steep cliff appears before you, falling\ninto the darkness. Ahead to the north, a light flickers in\nthe distance, but there is no way across the chasm.",
+      n_to: -1,
+      s_to: 496,
+      e_to: -1,
+      w_to: -1,
+      loc_x: 129,
+      loc_y: 20
+    },
+    {
+      title: "East Title",
+      description: "East Desc",
+      n_to: 1125,
+      s_to: 0,
+      e_to: 0,
+      w_to: 496,
+      loc_x: 129,
+      loc_y: 20
+    },
+    {
+      title: "North Title",
+      description: "North Desc",
+      n_to: 1130,
+      s_to: 1124,
+      e_to: 1136,
+      w_to: 1137,
+      loc_x: 129,
+      loc_y: 21
+    },
+    {
+      title: "West Title",
+      description: "West Desc",
+      n_to: 1127,
+      s_to: 0,
+      e_to: 496,
+      w_to: 0,
+      loc_x: 127,
+      loc_y: 20
+    },
+    {
+      title: "North Title",
+      description: "North Desc",
+      n_to: 1128,
+      s_to: 1126,
+      e_to: 1129,
+      w_to: 1133,
+      loc_x: 127,
+      loc_y: 21
+    },
+    {
+      title: "North Title",
+      description: "North Desc",
+      n_to: 1156,
+      s_to: 1127,
+      e_to: 1178,
+      w_to: 0,
+      loc_x: 127,
+      loc_y: 22
+    },
+    {
+      title: "East Title",
+      description: "East Desc",
+      n_to: 1132,
+      s_to: 1139,
+      e_to: 1144,
+      w_to: 1127,
+      loc_x: 128,
+      loc_y: 21
+    },
+    {
+      title: "North Title",
+      description: "North Desc",
+      n_to: 1131,
+      s_to: 1125,
+      e_to: 1151,
+      w_to: 1161,
+      loc_x: 129,
+      loc_y: 22
+    },
+    {
+      title: "North Title",
+      description: "North Desc",
+      n_to: 1135,
+      s_to: 1130,
+      e_to: 1140,
+      w_to: 1152,
+      loc_x: 129,
+      loc_y: 23
+    },
+    {
+      title: "North Title",
+      description: "North Desc",
+      n_to: 1166,
+      s_to: 1129,
+      e_to: 1208,
+      w_to: 0,
+      loc_x: 128,
+      loc_y: 22
+    },
+    {
+      title: "West Title",
+      description: "West Desc",
+      n_to: 1134,
+      s_to: 1141,
+      e_to: 1127,
+      w_to: 1147,
+      loc_x: 126,
+      loc_y: 21
+    },
+    {
+      title: "North Title",
+      description: "North Desc",
+      n_to: 1146,
+      s_to: 1133,
+      e_to: 1149,
+      w_to: 1157,
+      loc_x: 126,
+      loc_y: 22
+    }
+  ]
+};

a34a90768c1311a86d0094d6044e4a6129b7ea0a
Kristin Barr <kristinbarr@gmail.com>
2020-03-05 12:03:48 -0500

    handle move function

--- awesome-mud/src/components/game/Controls.js
+++ awesome-mud/src/components/game/Controls.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
-import controls from "../game/assets/Window.png"
 import styled from "styled-components"
 
 import { move_room } from "../../actions"
 
 const Controls = props => {
-    return(
+    
+    const handleMove = e => {
+        e.preventDefault()
+        props.move_room(e.target.name)
+    }
+    
+    return (
         <ControlsContainer>
-            <img src={controls} alt="controls" />
+            <NorthButton name="n" onClick={handleMove} >N</NorthButton>
+            <div>
+                <WestButton name="w" onClick={handleMove} >W</WestButton>
+                <EastButton name="e" onClick={handleMove} >E</EastButton>
+            </div>
+            <SouthButton name="s" onClick={handleMove} >S</SouthButton>
         </ControlsContainer>
     )
 }
@@ -31,6 +41,62 @@ const ControlsContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
     font-size: 2rem;
     padding: 2rem;
 `
+const NorthButton = styled.button`
+    margin: .5rem 0;
+    padding: .6rem 1rem;
+    color: #DDD;
+    background: #222;
+    border-right: 3px solid #DDD;
+    border-bottom: 3px solid #DDD;
+    font-size: 2rem;
+    &:hover {
+        color: #222;
+        background: #DDD;
+    }
+`
+const WestButton = styled.button`
+    margin: 0 2rem;
+    padding: .6rem .8rem;
+    color: #DDD;
+    background: #222;
+    border-right: 3px solid #DDD;
+    border-bottom: 3px solid #DDD;
+    font-size: 2rem;
+    &:hover {
+        color: #222;
+        background: #DDD;
+    }
+`
+const EastButton = styled.button`
+    margin: 0 2rem;
+    padding: .6rem 1rem;
+    color: #DDD;
+    background: #222;
+    border-right: 3px solid #DDD;
+    border-bottom: 3px solid #DDD;
+    font-size: 2rem;
+    &:hover {
+        color: #222;
+        background: #DDD;
+    }
+`
+const SouthButton = styled.button`
+    margin: .5rem 0;
+    padding: .6rem 1rem;
+    color: #DDD;
+    background: #222;
+    border-right: 3px solid #DDD;
+    border-bottom: 3px solid #DDD;
+    font-size: 2rem;
+    &:hover {
+        color: #222;
+        background: #DDD;
+    }
+`
+// const MiddleButtons = styled.div`
+//     margin: 0;
+// `

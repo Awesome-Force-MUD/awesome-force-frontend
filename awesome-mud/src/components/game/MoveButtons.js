7e85520d1a2e312634bd5f509ac7951eacb87dc4
Kristin Barr <kristinbarr@gmail.com>
2020-03-06 12:49:53 -0500

    updates

--- awesome-mud/src/components/game/MoveButtons.js
+++ awesome-mud/src/components/game/MoveButtons.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { move_room } from '../actions'
+import { move_room } from '../../actions'
 
 const MoveButtons = props => {
 
